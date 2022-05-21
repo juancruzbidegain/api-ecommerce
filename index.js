aebf4257ad1aec68e7c5532926b9a3720bc5bbbf
Juan Cruz <juancruzbidegain@gmail.com>
2022-05-21 18:06:34 -0300

    add params route

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const $PORT = 8080;
 
-app.get("/", (req, res) =>{
+
+
+app.get("/", (req, res) => {
   res.send("SV Running");
 });
 
-app.listen(port, () =>{
-  console.log("My port: " + port);
+
+app.get("/products", (req, res) => {
+  res.json([
+  {
+    name: 'Laptop Gamer',
+    price: 23000,
+  },
+  {
+    name: 'iPhone X3',
+    price: 32000,
+  }
+  ]);
+})
+
+app.get("/products/:id", (req, res) => {
+  const { id }  = req.params
+  res.json({id:id ,name: 'Laptop Gamer',price: 23000, })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+      categoryId,
+      productId
+  });
+});
+
+
+app.listen($PORT, () => {
+  console.log(`Server running on PORT: ${$PORT}`);
 });
